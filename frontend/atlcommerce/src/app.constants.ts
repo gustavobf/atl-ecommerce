7aecdf51164fee24735c8e3336c67ca8578fd441
Gustavo Batista <gustavobatistafig@hotmail.com>
2023-10-26 22:53:30 -0300

    ajustes env var

--- frontend/atlcommerce/src/app.constants.ts
+++ frontend/atlcommerce/src/app.constants.ts
@@ -1,3 +1,3 @@
 import { environment } from "./environments/environment.prod";
 
-export const API_ENDPOINT = environment.API_ENDPOINT_AMBIENTE ?? 'http://localhost:8080';
+export const API_ENDPOINT = environment.API_ENDPOINT_AMBIENTE_APP ?? 'http://localhost:8080';
--- frontend/atlcommerce/src/environments/environment.prod.ts
+++ frontend/atlcommerce/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_ENDPOINT_AMBIENTE: ''
+  API_ENDPOINT_AMBIENTE_APP: process.env.API_ENDPOINT_AMBIENTE
 };
